4ad669bfd40a1684770932e71796ea907a439022
snowflake4950 <117202238+snowflake4950@users.noreply.github.com>
2022-11-01 19:34:54 +0200

    Update streamlit_app.py
    Add new text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 and Blueberry oatmeal')
+streamlit.text('Kale and spinach')
+streamlit.text('Hard boil fre range eggs')
